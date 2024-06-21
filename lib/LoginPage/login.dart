a9c79552e1449d9cd74dc632dfc1c8b8bff512a0
VladLavrynenko <vlad.lavrynenko@powercoders.org>
2024-06-21 15:10:29 +0200

    Adding AuthFirebase. Dependencies.

---
+++ lib/LoginPage/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // FirebaseAuth.instance
+    //     .authStateChanges()
+    //     .listen((User? user) {
+    //   if (user != null) {
+    //     print(user.uid);
+    //   }
+    // });
+
+    return const Placeholder();
+  }
+}
